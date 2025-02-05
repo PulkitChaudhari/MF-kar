6f57aed6e19d79e1714ef6b7a5affe783c6e532f
PulkitChaudhari <pulkitchaudhari@gmail.com>
2025-02-05 17:36:49 +0530

    FEAT: introduced backfill and update cron

---
+++ mf-karr-backend/scripts/dbDataBackfill.py
@@ -0,0 +1,85 @@
+import requests
+import json
+import time
+from allMfData import mf_data
+from constants import SORTED_RESULTS
+import psycopg2
+from datetime import datetime
+
+# Connect to PostgreSQL database
+conn = psycopg2.connect(
+    dbname='postgres',
+    user='admin',
+    password='admin',
+    host='localhost',
+    port='5432'
+)
+cursor = conn.cursor()
+
+# Constants
+API_BASE_URL = "https://api.mfapi.in/mf/"
+# DELAY_SECONDS = float(sys.argv[0]) # Delay between requests to avoid rate limiting
+DELAY_SECONDS = 0
+
+def fetch_latest_mf_data(scheme_code: int) -> dict:
+    try:
+        url = f"{API_BASE_URL}{scheme_code}"
+        print(f"Fetching data for scheme: {scheme_code}")
+        
+        response = requests.get(url)
+        response.raise_for_status()
+        
+        time.sleep(DELAY_SECONDS)  # Add delay between requests
+        return response.json()
+        
+    except requests.RequestException as e:
+        error_msg = f"Error fetching scheme {scheme_code}: {str(e)}"
+        print(error_msg)
+
+def main():
+    scheme_codes = [mf['schemeCode'] for mf in mf_data]
+    print(f"Starting to fetch latest data for mutual fund schemes...")
+    for scheme_code in scheme_codes:
+
+        results = fetch_latest_mf_data(scheme_code)
+        data = results['data']
+
+        for keys in SORTED_RESULTS: 
+            if (SORTED_RESULTS[keys][0] <= scheme_code and SORTED_RESULTS[keys][1] >= scheme_code): 
+                tablename = keys[0],keys[1]
+
+        cursor.execute(f"""
+            SELECT MAX(nav_date) FROM mf_data_results_{tablename[0]}_{tablename[1]} where fund_id={scheme_code}
+        """)
+        results = cursor.fetchall()
+        if results and results[0][0] is not None:
+            lastAvailableDate = results[0][0] 
+            lastAvailableDate = str(lastAvailableDate)
+            year, month, day = lastAvailableDate.split('-')
+            lastAvailableDate = datetime(int(year), int(month), int(day))
+            print(lastAvailableDate)
+        else:
+            print(f"""No last max date found for %s""",scheme_code)
+            break
+
+        for nav_data in data:
+            date = nav_data['date']
+            nav = nav_data['nav']
+            datetime_object = datetime.strptime(date, '%d-%m-%Y')  # Assuming date is in 'dd-mm-yyyy' format
+            year = datetime_object.year
+            month = datetime_object.month
+            day = datetime_object.day
+            if (lastAvailableDate >= datetime_object):
+                 break
+            cursor.execute(f"""
+            INSERT INTO mf_data_results_{tablename[0]}_{tablename[1]} 
+            (nav_date, nav_value, fund_id) VALUES (TO_DATE(%s, 'dd-mm-yyyy'), %s, %s)
+            """, (str(date)+"-"+str(month)+"-"+str(year), nav, scheme_code))
+            print(f"""Insertion for mf_data_results_{keys[0]}_{keys[1]} completed.""")
+    conn.commit()
+    cursor.close()
+    conn.close()
+            
+
+if __name__ == "__main__":
+    main()
