20e45dabdf2d417d2c56ff620e37068a48303949
PulkitChaudhari <pulkitchaudhari@gmail.com>
2025-04-04 16:21:46 +0530

    Added missing dependency in backend and type in frontend env variables

--- mf-karr/app/api/auth/[...nextauth]/route.tsx
+++ mf-karr/app/api/auth/[...nextauth]/route.tsx
@@ -6,12 +6,12 @@ const handler = NextAuth({
   providers: [
     // OAuth authentication providers...
     GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+      clientId: process.env.AUTH_GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.AUTH_GITHUB_CLIENT_SECRET as string,
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: process.env.AUTH_GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET as string,
     }),
   ],
 });
